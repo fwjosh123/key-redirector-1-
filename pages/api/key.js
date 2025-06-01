655dca6b24af60f4e6071ad72b277b500582b122
fwjosh123 <jarrieta0713@gmail.com>
2025-06-01 19:31:08 -0400

    Update key.js

--- pages/api/key.js
+++ pages/api/key.js
@@ -1,4 +1,10 @@
 export default function handler(req, res) {
-  const key = 'TJ-' + Math.random().toString(36).substr(2, 16).toUpperCase();
+  res.setHeader('Cache-Control', 'no-store');
+
+  const key = 'TJ-' + Array(16)
+    .fill(0)
+    .map(() => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'[Math.floor(Math.random() * 36)])
+    .join('');
+
   res.status(200).json({ key });
 }
