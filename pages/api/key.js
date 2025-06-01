2c246c86b20d9e4268673baf422afb9e7f555bc7
fwjosh123 <jarrieta0713@gmail.com>
2025-06-01 19:34:57 -0400

    Update key.js

--- pages/api/key.js
+++ pages/api/key.js
@@ -1,8 +1,10 @@
 export default function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
 
-  const key = 'TJ-' + Array(16)
-    .fill(0)
+  const key = 'TJ-' + [...Array(16)]
     .map(() => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'[Math.floor(Math.random() * 36)])
     .join('');
 
