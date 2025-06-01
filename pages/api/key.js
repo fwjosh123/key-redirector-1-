667c666b9668c7e62d04dc27a9ff3cfeaf75a1b5
fwjosh123 <jarrieta0713@gmail.com>
2025-06-01 19:27:49 -0400

    Update key.js

--- pages/api/key.js
+++ pages/api/key.js
@@ -1,6 +1,4 @@
 export default function handler(req, res) {
-  // Generate a random 16-digit key starting with TJ-
-  const key = 'TJ-' + Math.random().toString(36).substring(2, 10).toUpperCase() +
-                     Math.random().toString(36).substring(2, 10).toUpperCase();
-  res.status(200).json({ key: key.substring(0, 16) });
+  const key = 'TJ-' + Math.random().toString(36).substr(2, 16).toUpperCase();
+  res.status(200).json({ key });
 }
